d36e333c44e95d90198ebce15985dd36c5191a4b
Olha <olha.hnatetska.pz.2022@lpnu.ua>
2024-11-28 23:34:02 +0200

    finished a lab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ToastNotificationComponent } from './components/toast-notification/toast-notification.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    NavBarComponent,
+    ToastNotificationComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'lab5';
+  title = 'insurance company';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ClientsTableComponent } from './components/clients-table/clients-table.component';
+import { PoliciesTableComponent } from './components/policies-table/policies-table.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'clients', component: ClientsTableComponent },
+  { path: 'policies', component: PoliciesTableComponent },
+  { path: '', redirectTo: '/clients', pathMatch: 'full' },
+  { path: '**', redirectTo: '/clients' },
+];
